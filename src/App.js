8e065390f763a2d9ccc63aea3b6707aacdbd73cb
Antonio Jack <jack@ubuntu.com>
2018-06-25 14:10:25 +0700

    Add Gestalt layout and buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { Component } from "react";
-import { connect } from 'react-redux';
+import React, {Component} from "react";
+import {connect} from 'react-redux';
+
+import {Box, Column, Button} from 'gestalt';
+import 'gestalt/dist/gestalt.css';
 
 import Canvas from './containers/Canvas'
 import data from './store/btnLookup.json';
@@ -7,18 +10,30 @@ import data from './store/btnLookup.json';
 class App extends Component {
     render() {
         return (
-            <div>
-                {data.rectangles.map(rectangle => <button onClick={() => this.props.addRect(rectangle.name,rectangle.color)}>{rectangle.name}</button>)}
-                {/* <button onClick={() => this.props.addRect('blue')}>Blue</button> */}
-                <Canvas />
-            </div>
+            <Box display="flex" direction="row" paddingY={2}>
+                <Column span={3}>
+                    <Box color="lightGray" padding={1}>
+                        <Box color="white" paddingY={2}>
+                            {data.rectangles.map(rectangle => <Button inline color={rectangle.color} text={rectangle.name}
+                                                                      onClick={() => this.props.addRect(rectangle.name, rectangle.color)}/>)}
+                        </Box>
+                    </Box>
+                </Column>
+                <Column span={9}>
+                    <Box color="lightGray" padding={1}>
+                        <Box color="white" paddingY={2}>
+                            <Canvas/>
+                        </Box>
+                    </Box>
+                </Column>
+            </Box>
         );
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        addRect: (name, color) => dispatch({ 'type': 'ADD_RECT', 'name': name, 'color': color })
+        addRect: (name, color) => dispatch({'type': 'ADD_RECT', 'name': name, 'color': color})
     }
 };
 
