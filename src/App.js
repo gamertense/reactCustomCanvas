4d847c7072d065aab465ed7d5bb8126d79deb172
Antonio Jack <jack@ubuntu.com>
2018-06-25 14:59:44 +0700

    Reorganize code structure (make it easier to read)

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import data from './store/btnLookup.json';
 
 class App extends Component {
     render() {
+        const btnList = data.rectangles.map(rectangle => <Button inline color={rectangle.color} text={rectangle.name}
+                                                                 onClick={() => this.props.addRect(rectangle.name, rectangle.color)}/>);
         return (
             <Box display="flex" direction="row" paddingY={2}>
                 <Column span={3}>
                     <Box color="lightGray" padding={1}>
                         <Box color="white" paddingY={2}>
-                            {data.rectangles.map(rectangle => <Button inline color={rectangle.color} text={rectangle.name}
-                                                                      onClick={() => this.props.addRect(rectangle.name, rectangle.color)}/>)}
+                            {btnList}
                         </Box>
                     </Box>
                 </Column>
