e026a2d40d0d7721173f687a6009f6bbc0aeb348
Antonio Jack <jack@ubuntu.com>
2018-06-27 14:38:06 +0700

    Add handleChange function.

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,19 +1,21 @@
-import React, {Component} from "react";
-import {connect} from 'react-redux';
-import {Button, Col, Row} from 'reactstrap';
+import React, {Component} from "react"
+import {connect} from 'react-redux'
+import {Button, Col, Row, Container, InputGroup, InputGroupAddon, Input} from 'reactstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-import data from '../../store/btnLookup.json';
-import './Sidebar.css';
+import data from '../../store/btnLookup.json'
+import './Sidebar.css'
 
 class Sidebar extends Component {
     constructor(props) {
         super(props);
         this.count = 0;
-    }
+        this.state = {
+            textInput: ''
+        };
 
-    state = {
-        textInput: ''
-    };
+        this.handleChange = this.handleChange.bind(this);
+    }
 
     showButton = button => {
         this.count += 1;
@@ -23,18 +25,27 @@ class Sidebar extends Component {
         const txtIn = this.state.textInput.toLowerCase();
         const btnid = Math.random().toString(36).substr(2, 9);
         const btnName = button.name.toLowerCase();
-        const style = {background: button.color};
         if (btnName.startsWith(txtIn))
-            return <Col xs="6" className="Button-Col" key={btnid}><Button style={style}
-                                                                          onClick={() => this.props.addRect(button.name, button.color)}>{button.name}</Button></Col>
+            return <Col xs="12" md="6" className="Button-Col" key={btnid}><Button
+                onClick={() => this.props.addRect(button.name, button.color)}>{button.name}</Button></Col>
     };
+
+    handleChange(event) {
+        this.count = 0;
+        this.setState({textInput: event.target.value});
+    }
 
     render() {
         const btnList = data.rectangles.map(rectangle => this.showButton(rectangle));
         return (
-            <Row>
-                {btnList}
-            </Row>
+            <Container className="Sidebar-container">
+                <InputGroup>
+                    <Input placeholder="Search" onChange={this.handleChange} value={this.state.textInput}/>
+                </InputGroup>
+                <Row>
+                    {btnList}
+                </Row>
+            </Container>
         );
     }
 }
