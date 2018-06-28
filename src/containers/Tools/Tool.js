a4b65b347185e70c110f67525e8ad0aac0424145
Antonio Jack <jack@ubuntu.com>
2018-06-28 16:09:08 +0700

    Add onBtnClick handler

--- src/containers/Tools/Tool.js
+++ src/containers/Tools/Tool.js
@@ -5,12 +5,22 @@ import {Button} from 'reactstrap';
 import './Tool.css';
 
 class Tool extends Component {
+    onBtnClick = action => {
+        this.props.updateTransform('remove');
+        this.props.updateTransform('add');
+
+        if (action === 'remove')
+            this.props.remove();
+        else
+            this.props.clearCanvas();
+    };
+
     render() {
         return (
             <div className="Tool">
-                <Button className="Button" color="warning" onClick={this.props.remove}><i
+                <Button className="Button" color="warning" onClick={() => this.onBtnClick('remove')}><i
                     className="fas fa-eraser"></i> Remove</Button>
-                <Button className="Button" color="danger" onClick={this.props.clearCanvas}><i
+                <Button className="Button" color="danger" onClick={() => this.onBtnClick('clear')}><i
                     className="fas fa-trash-alt"></i> Clear Canvas</Button>
             </div>
         );
@@ -20,7 +30,8 @@ class Tool extends Component {
 const mapDispatchToProps = dispatch => {
     return {
         remove: () => dispatch({'type': 'REMOVE'}),
-        clearCanvas: () => dispatch({'type': 'CLEAR_CANVAS'})
+        clearCanvas: () => dispatch({'type': 'CLEAR_CANVAS'}),
+        updateTransform: (action) => dispatch({'type': 'UPDATE_TRANSFORM', 'action': action})
     }
 };
 
