e258e9b5dc2848ae8f74a0d20299dbcae23c148e
Antonio Jack <gamertense@live.com>
2018-07-03 22:49:14 +0700

    Add Redux store and Redux Dev Tools association.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,7 +10,20 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            // console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
 
-ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
