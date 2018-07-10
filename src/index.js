74d0a15081b4299e6474973d5fe4224602a2d856
Antonio Jack <jack@ubuntu.com>
2018-07-10 11:11:54 +0700

    Code restructuring.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import './index.css';
@@ -10,21 +10,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const logger = store => {
-    return next => {
-        return action => {
-            /*console.log('[Middleware] Dispatching', action);
-            const result = next(action);
-            console.log('[Middleware] next state', store.getState());
-            return result;*/
-            return next(action);
-        }
-    }
+const logger = store => next => action => {
+    // console.log('[Middleware] Dispatching', action);
+    // const result = next(action);
+    // console.log('[Middleware] next state', store.getState());
+    // return result;
+    return next(action);
 };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
