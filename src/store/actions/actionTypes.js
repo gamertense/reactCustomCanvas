f5d8fbf97d79fefb2fc42ab82ef759573b3fbd1d
Antonio Jack <jack@ubuntu.com>
2018-07-14 16:41:03 +0700

    Remove unused action type.

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -2,7 +2,6 @@ export const ADD_RECT = 'ADD_RECT';
 export const REMOVE = 'REMOVE';
 export const CLEAR_CANVAS = 'CLEAR_CANVAS';
 export const UPDATE_LOCATION = 'UPDATE_LOCATION';
-export const UPDATE_SCALE = 'UPDATE_SCALE';
 export const UPDATE_TRANSFORM = 'UPDATE_TRANSFORM';
 export const UPDATE_SELECTED = 'UPDATE_SELECTED';
 export const UPDATE_TOOLTIP = 'UPDATE_TOOLTIP';
